e368715940d3f8d81dc638cbcb08bd66acc995ee
iflocky <Petrenko1702@gmail.com>
2025-01-29 15:28:09 +0300

    feat: add list page

--- sportlux/main/urls.py
+++ sportlux/main/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.popular_list, name='popular_list'),
-    path('<slug:slug>/', views.product_detail, name='product_detail')
+    path('shop/', views.product_list, name='product_list'),
+    path('shop/category/<slug:category_slug>', views.product_list, name='product_list_by_category'),
+    path('shop/<slug:slug>', views.product_detail, name='product_detail')
 ]
--- sportlux/main/views.py
+++ sportlux/main/views.py
@@ -8,4 +8,21 @@ def popular_list(request):
 
 def product_detail(request,slug):
   product = get_object_or_404(Product, slug=slug, available=True)
-  return (request, 'main/product/detail.html', {'product': product})
+  return render(request, 'main/product/detail.html', {'product': product})
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    context = {
+        'category': category,
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'main/product/list.html', context)
